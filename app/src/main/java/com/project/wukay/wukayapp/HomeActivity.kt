ce846c95756e0090f712446a82f4c627cbb9a81e
LeoVgr <leo.vgr@hotmail.com>
2018-11-03 15:20:36 +0100

    debut page level

--- app/src/main/java/com/project/wukay/wukayapp/HomeActivity.kt
+++ app/src/main/java/com/project/wukay/wukayapp/HomeActivity.kt
@@ -1,5 +1,6 @@
 package com.project.wukay.wukayapp
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_home.*
@@ -9,5 +10,11 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        boutonCommencerAventure.setOnClickListener{
+            val next = Intent(this@HomeActivity, LevelsActivity::class.java)
+            startActivity(next);
+        }
     }
+
 }
