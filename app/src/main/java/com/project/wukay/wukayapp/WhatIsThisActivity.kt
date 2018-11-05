3d451ff8af501841419e94c08aca898a89d35d04
Eliott BALDY <kakou8700@gmail.com>
2018-11-05 19:47:14 +0100

    Trouve L'animal 1.0 ajout liste animaux simple

--- app/src/main/java/com/project/wukay/wukayapp/WhatIsThisActivity.kt
+++ app/src/main/java/com/project/wukay/wukayapp/WhatIsThisActivity.kt
@@ -2,11 +2,32 @@ package com.project.wukay.wukayapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_what_is_this.*
+import java.util.*
 
 class WhatIsThisActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_what_is_this)
+
+
+
+
+        val animalArray = arrayOf( R.drawable.cochon,R.drawable.vache, R.drawable.girafe)
+
+
+
+        startButton.setOnClickListener {
+
+                val r = Random()
+
+                val n = r.nextInt(3)
+
+                animalPic.setImageResource(animalArray[n])
+
+
+        }
+
     }
 }
