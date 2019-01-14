4f23caeffc15ea672e2844397a308fa858ce377c
LeoVgr <leo.vgr@hotmail.com>
2019-01-14 17:11:31 +0100

    Début jeux feed animals

---
+++ app/src/main/java/com/project/wukay/wukayapp/FeedAnimalsControler.kt
@@ -0,0 +1,14 @@
+
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class FeedAnimalsControler : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+}
---
+++ app/src/main/java/com/project/wukay/wukayapp/metier/FeedAnimalsModel.kt
@@ -0,0 +1,5 @@
+package com.project.wukay.wukayapp.metier
+
+class FeedAnimalsModel {
+
+}
