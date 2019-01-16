c8fb7f6943b96c7926c5c33fc3b0e61bcd79f04b
Eliott BALDY <kakou8700@gmail.com>
2019-01-16 10:32:45 +0100

    réécriture de lifePopActivity

--- app/src/main/java/com/project/wukay/wukayapp/LifePopActivity.kt
+++ app/src/main/java/com/project/wukay/wukayapp/LifePopActivity.kt
@@ -1,13 +1,10 @@
 package com.project.wukay.wukayapp
 
-import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.DisplayMetrics
 import android.view.Gravity
-import android.view.WindowManager
 import android.view.WindowManager.LayoutParams
-import android.widget.Button
 import kotlinx.android.synthetic.main.activity_life_pop.*
 
 class LifePopActivity : AppCompatActivity() {
@@ -25,7 +22,7 @@ class LifePopActivity : AppCompatActivity() {
         val intent = intent
 
         var nbLife = intent.getStringExtra("nbLifeToShow")
-        System.out.print("NBLIFE POPUP = " + nbLife)
+        System.out.print("NBLIFE POPUP = $nbLife")
 
 
         closeButton.setOnClickListener {
@@ -34,18 +31,16 @@ class LifePopActivity : AppCompatActivity() {
 
         lifeWonTxt.text= "+$nbLife"
 
-        var metrics: DisplayMetrics = DisplayMetrics()
+        var metrics = DisplayMetrics()
 
         windowManager.defaultDisplay.getMetrics(metrics)
 
-        var width = metrics.widthPixels
-        var height = metrics.heightPixels
+        metrics.widthPixels
+        metrics.heightPixels
 
         window.setLayout(WIDTH, HEIGHT)
 
-        var params: LayoutParams  =  LayoutParams()
-
-        params = window.attributes
+        var params: LayoutParams = window.attributes
 
         params.gravity =Gravity.CENTER
         params.x = 0
