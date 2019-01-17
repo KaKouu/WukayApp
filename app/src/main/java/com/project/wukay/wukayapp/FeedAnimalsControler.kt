f89310a467bbd4a6b5dcf6e33ef628d8eb8ba686
Cedric-Duraysseix <cedric.duraysseix@etu.unilim.fr>
2019-01-17 10:06:21 +0100

    Ajout des images dans HideAnimal et FeedAnimal

--- app/src/main/java/com/project/wukay/wukayapp/FeedAnimalsControler.kt
+++ app/src/main/java/com/project/wukay/wukayapp/FeedAnimalsControler.kt
@@ -29,23 +29,6 @@ class FeedAnimalsControler : AppCompatActivity() {
         }
 
         val listOfAnimals = arrayOf(
-            R.drawable.animaux_cochon_silhouette,
-            R.drawable.animaux_vache_silhouette,
-            R.drawable.animaux_girafe_silhouette,
-            R.drawable.animaux_bouc_silhouette,
-            R.drawable.animaux_chat_silhouette,
-            R.drawable.animaux_piranha_silhouette,
-            R.drawable.animaux_morse_silhouette,
-            R.drawable.animaux_panda_silhouette,
-            R.drawable.animaux_paon_silhouette,
-            R.drawable.animaux_pingouin_silhouette,
-            R.drawable.animaux_poule_silhouette,
-            R.drawable.animaux_tigre_silhouette,
-            R.drawable.animaux_zebre_silhouette,
-            R.drawable.animaux_lion_silhouette
-        )
-
-        val listOfFood = arrayOf(
             R.drawable.animaux_cochon,
             R.drawable.animaux_vache,
             R.drawable.animaux_girafe,
@@ -60,6 +43,23 @@ class FeedAnimalsControler : AppCompatActivity() {
             R.drawable.animaux_tigre,
             R.drawable.animaux_zebre,
             R.drawable.animaux_lion
+        )
+
+        val listOfFood = arrayOf(
+            R.drawable.food_mais,
+            R.drawable.icon_herbe,
+            R.drawable.icon_feuille,
+            R.drawable.icon_herbe,
+            R.drawable.food_souris,
+            R.drawable.food_poisson,
+            R.drawable.food_crevette,
+            R.drawable.food_bambou,
+            R.drawable.food_graine,
+            R.drawable.food_poisson,
+            R.drawable.food_graine,
+            R.drawable.food_viande,
+            R.drawable.icon_feuille,
+            R.drawable.food_viande
         )
         feedAnimalsModel.generateAnimalsAndFood()
         animalFeed1.setImageResource(listOfAnimals[feedAnimalsModel.getBoardOfAnimals()[0]])
@@ -151,9 +151,6 @@ class FeedAnimalsControler : AppCompatActivity() {
                     }
                 }
             }
-
-
-
         }
     }
 }
